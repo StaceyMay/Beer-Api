7097e0bd86dadc2436e11afba7e0964db5cacd39
staceymay <missstaceymay@gmail.com>
2016-12-13 21:09:26 -0800

    Linked Trips to Places

---
+++ app/controllers/beers_controller.rb
@@ -0,0 +1,31 @@
+class v1::BeersController < ApplicationController
+
+  def index
+    @beers = Beer.all
+  end
+
+  def show
+    @beer = Beer.find_by(id: params[:id])
+  end
+
+  def create
+    @beer = Beer.create(name: params[:name], alcohol_content: params[:alcohol_content] , style: params[:style] , malt: params[:malt] , hop: params[:hop])
+
+    render :show
+  end
+
+  def update
+    @beer = Beer.find_by(id: params[:id])
+    @beer.assign_attributes(name: params[:name], alcohol_content: params[:alcohol_content] , style: params[:style] , malt: params[:malt] , hop: params[:hop])
+    @beer.save
+
+    render :show
+  end
+
+  def destroy
+    @beer = Beer.find_by(id: params[:id])
+    @beer.destroy
+
+    render json: {message: "Beer #{params[:id]} successfully deleted!"}
+  end
+end
